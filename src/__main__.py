d290b65a8ebe88ef110e68e654a2c247e1692223
alexjvr1 <alexjvr@gmail.com>
2024-04-18 15:04:35 +0100

    Changed is to '==' in args.mask == country

--- src/__main__.py
+++ src/__main__.py
@@ -5,13 +5,13 @@
 path_to_raster = "example/"
 
 # Import modules and initiate instances of each class
-# from readarguments import ReadArguments
+from readarguments import ReadArguments
 from importmap import ImportMap
 
 # from colourmap import ColourMap
 # from samplestomap import SamplesToMap
 
-# ReadArguments = ReadArguments()
+ReadArguments = ReadArguments()
 ImportMap = ImportMap()
 # ColourMap = ColourMap()
 # SamplesToMap = SamplesToMap()
@@ -21,6 +21,10 @@ ImportMap = ImportMap()
 def main():
     # Import all arguments. Now we can call the arguments as args.argument
     args = ReadArguments.getargs()
+    if args.mask == "country" and args.country is None:
+        parser.error(
+            "if --mask country is chosen, a country name needs to be provided with --country"
+        )
     # Import raster file and return the 'mosaic' variable of all the tiles stitched together
     # Save mosaic.tif, and save a plot (.png) of the mosaic map to the output folder
     # Options: raster dir, outdir
@@ -28,12 +32,13 @@ def main():
 
     # Create a mask to delimit the map (ImportMap)
     # If --country option is selected:
-    if args.mask == 'country': 
+    if args.mask == "country":
         # Use function to produce a country mask
-        ImportMap.mask_map_by_country(args.mask)
+        ImportMap.mask_map_by_country(args.country)
     # Else coordinates are selected
-    else args.mask == 'coords':
-        ImportMap.mask_map_by_coords(args.coords)
+    elif args.mask == "coords":
+        print("Coordinates chosen")
+        # ImportMap.mask_map_by_coords(args.coords)
         # Use function to use coordinates to create mask
     # Save figure (.png)
     # Return mask variable
