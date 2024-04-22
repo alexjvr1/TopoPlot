d9127542247ddd4a222f0410c15055457d918583
alexjvr1 <alexjvr@gmail.com>
2024-04-22 13:52:27 +0100

    create a statement based on if country and/or coords are provided to mask the map

--- src/__main__.py
+++ src/__main__.py
@@ -39,20 +39,37 @@ mosaic = ImportMap.import_and_merge_raster_file(
 )
 print("Merged raster file (*.tif) and figure (*.png) saved to", outdir)
 
-if bool(args.coords) == True:
-    print(args.coords)
-
 # Create a mask to crop the map and show only the region of interest (ImportMap)
 # If --country option is selected:
-if args.mask == "country":
+if args.mask == "country" and bool(args.coordinates) == True:
     # Use function to clip map by country mask. Return the masked image and a
     # value range for the elevation assigned to each cell in the map
+    print(
+        "Clipping raster with mask for",
+        args.country,
+        "and clip map to include only",
+        args.coordinates,
+    )
+    out_img, value_range = ImportMap.mask_map_by_country(
+        indir=args.indir, country=args.country, outdir=args.outdir
+    )
+    print("Clipped", args.country, "raster (*.tif) and figure (*.png) saved to", outdir)
+    # And clip using bounding box specified by coordinates
+    print(
+        "Clipped",
+        args.country,
+        "raster (*.tif) using the bounding box",
+        args.coordinates,
+    )
+    # out_img, value_range = ImportMap.mask_map_by_coords()
+# Else plot only the country
+elif args.mask == "country" and bool(args.coordinates) == False:
     print("Clipping raster with mask for", args.country)
     out_img, value_range = ImportMap.mask_map_by_country(
         indir=args.indir, country=args.country, outdir=args.outdir
     )
-    print("Clipped raster (*.tif) and figure (*.png) saved to", outdir)
-    # Else coordinates are selected
+    print("Clipped", args.country, "raster (*.tif) and figure (*.png) saved to", outdir)
+# Else mask the map using the bounding box created by the coordinates only.
 elif args.mask == "coords":
     print("Coordinates chosen")
 # ImportMap.mask_map_by_coords(args.coords)
