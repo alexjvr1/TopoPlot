d62ddbcf4c42bc5e4fa9f12bf15a7cb39dc3fa06
alexjvr1 <alexjvr@gmail.com>
2024-07-02 11:04:30 +0100

    update changes

--- src/colourmap.py
+++ src/colourmap.py
@@ -6,6 +6,11 @@ from matplotlib import cm
 from matplotlib import colors
 from matplotlib.colors import ListedColormap
 import earthpy.spatial as es
+import pandas as pd
+from shapely.geometry import Point
+import geopandas as gpd
+from geopandas import GeoDataFrame
+import pickle
 
 
 # All functions related to colouring and adding a light source to the map
@@ -13,51 +18,48 @@ class ColourMap:
     def __init__(self):
         self = self
 
-    def plot_examples(self, colormaps):
-        # From Matplotlib documentation:
-        # Helper function to plot data with associated colormap.
-        np.random.seed(19680801)
-        data = np.random.randn(30, 30)
-        n = len(colormaps)
-        fig, axs = plt.subplots(
-            1, n, figsize=(n * 2 + 2, 3), layout="constrained", squeeze=False
-        )
-        for [ax, cmap] in zip(axs.flat, colormaps):
-            psm = ax.pcolormesh(data, cmap=cmap, rasterized=True, vmin=-4, vmax=4)
-            fig.colorbar(psm, ax=ax)
-        plt.show()
-
     # representing the range in elevation values for our map
-    def map_in_colour(self, colourgrad, value_range, clipped_array, azimuth, altitude):
+    def map_in_colour(
+        self,
+        colourgrad,
+        value_range,
+        clipped_array,
+        azimuth,
+        altitude,
+        fig_height,
+        fig_width,
+        outdir,
+        input,
+        samples,
+    ):
         # Rescale the colourgradient to the value_range
         colourgrad = mpl.colormaps[str(colourgrad)].resampled(value_range)
         newcolours = colourgrad(np.linspace(0, 1, 256))
+        # Add background colour: white
         background_colour_white = np.array([255 / 255, 255 / 255, 255 / 255, 1.0])
         newcolours = np.vstack((newcolours, background_colour_white))
+        # create the final colour gradient
         map_colourgrad = ListedColormap(newcolours)
-        """ # Plot
-        fig = plt.figure(facecolor="#FCF6F5FF")
-        fig.set_size_inches(7, 3.5)
-        ax = plt.axes()
-        plt.imshow(clipped_array[0], cmap=map_colourgrad)
-        ax.axis("off")
-        plt.show() """
+        # Define hillshade
         hillshade = es.hillshade(clipped_array[0], azimuth, altitude)
+        # plot the figure
         fig, ax = plt.subplots()
-        fig.set_size_inches(5, 5)
-        i = plt.imshow(clipped_array[0], cmap=map_colourgrad, norm=colors.LogNorm())
+        fig.set_size_inches(int(fig_width), int(fig_height))
+        plt.imshow(clipped_array[0], cmap=map_colourgrad, norm=colors.LogNorm())
         ax.imshow(hillshade, cmap="Greys", alpha=0.3)
         ax.axis("off")
         newax = fig.add_axes([0.79, 0.78, 0.08, 0.08], anchor="NE")
         newax.axis("off")
-        plt.show()
-
-    # and hillshade to the topography map
-
-    # Define the colour scale for the map
-
-    # Add hillshade to the map
-    def plot_hillshade(self, altitude, azimuth):
-        alt = altitude
-        azi = azimuth
-        return alt, azi
+        output_path = str(str(outdir) + "/colourmap")
+        plt.savefig(output_path + ".png")
+        plt.savefig(output_path + ".pdf")
+        pickle.dump(ax, open("myplot.pickle", "w"))
+        # Read in sample information
+        # sample_df = pd.read_table((str(input) + "/" + samples))
+        # Lat =
+        # Long =
+        # geometry = [Point(xy) for xy in zip(sample_df["Long"], sample_df["Lat"])]
+        # gdf = GeoDataFrame(sample_df, geometry=geometry)
+        # gdf.plot(ax=ax, marker="o", color="red", markersize=15)
+        # plt.show()
+        # return fig, ax
