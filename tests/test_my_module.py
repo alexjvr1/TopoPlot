e35ca19165a984af9d4260f94b10dbea650819a6
alexjvr1 <alexjvr@gmail.com>
2024-08-05 11:56:49 +0100

    add test folder

---
+++ tests/test_my_module.py
@@ -0,0 +1,14 @@
+#Tests for TopoPlot
+
+#Import modules
+import unittest
+
+#Create test class
+class MyTestCase(unittest.TestCase):
+
+    def test_function_name(self):
+
+        # Test code goes here
+        
+    pass
+
